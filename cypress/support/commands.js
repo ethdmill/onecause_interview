308b925f7c48203f00fa8f4685b9f03d48a200f2
Ethan Miller <ethdmill@gmail.com>
2021-04-21 15:15:03 -0400

    removed example custom command

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,19 +1,3 @@
-// const { ValueConverter } = require("@angular/compiler/src/render3/view/template")
-
-// example Custom Cypress command, obviously we don't need to log in to a math app
-Cypress.Commands.add('login', user => {
-    return cy
-      .request({
-        url: 'http://localhost:3000/login',
-        method: 'POST',
-        body: user,
-      })
-      .then(response => {
-        window.localStorage.setItem('token', response.body.user.token)
-        return {...response.body.user, ...user}
-      })
-  })
-
 // custom command I commonly write to make writing things just a bit quicker
 Cypress.Commands.add('getClick', (element) => {
   cy.get(element).click({ force: true })
